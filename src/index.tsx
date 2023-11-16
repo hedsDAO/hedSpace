defbd9f599883d754c75e4c71f028df23607d3a4
cgreer011 <camgreer01@gmail.com>
2023-11-16 13:28:40 -0600

    lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css';  // Assuming you have a CSS file for global styles
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 // Create a root constant to manage the root element rendering
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
