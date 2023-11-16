408414e98c4fd7be68fd71020f755821b8550b59
cgreer011 <camgreer01@gmail.com>
2023-11-15 22:16:31 -0600

    lazy load spline

--- src/Components/SplineObject.tsx
+++ src/Components/SplineObject.tsx
@@ -1,7 +1,18 @@
-import Spline from '@splinetool/react-spline';
+import React, { Suspense } from "react";
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
 
-const SplineObject = ({ splineUrl, onLoad }: { splineUrl: string, onLoad: () => void }) => {
-  return <Spline scene={splineUrl} onLoad={onLoad} />;
+const SplineObject = ({
+  splineUrl,
+  onLoad,
+}: {
+  splineUrl: string;
+  onLoad: () => void;
+}) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Spline scene={splineUrl} onLoad={onLoad} />;
+    </Suspense>
+  );
 };
 
 export default SplineObject;
