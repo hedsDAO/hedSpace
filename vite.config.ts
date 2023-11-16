dcf5363d92d206f8d84af8e2c0b5c63dd3a9b7b4
cgreer011 <camgreer01@gmail.com>
2023-11-16 13:23:04 -0600

    merge conflicts resolved

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   build: {
     rollupOptions: {
       output: {
@@ -11,6 +12,8 @@ export default defineConfig({
       },
     },
   },
+=======
+>>>>>>> 820b07a (new git)
   // base: '/hedSpace/',
   // root: './src'
 });
