7d5e394a33047bd032cc98e8ca2ce88d248db4f0
rahmteen <ramtin.khoee@gmail.com>
2024-02-07 12:26:51 -0800

    updated main with nothing

--- src/Store/constants.ts
+++
@@ -1,9 +0,0 @@
-export const EVENT_COMMENTS_API_PREFIX =
-  "https://eventcomments-v6adscuyxq-uc.a.run.app";
-export const GUEST_STATUS_API_PREFIX =
-  "https://gueststatus-v6adscuyxq-uc.a.run.app";
-export const MANGAGE_EVENT_API_PREFIX =
-  "https://manageevents-v6adscuyxq-uc.a.run.app";
-
-export const splineBgVideo1 =
-  "https://firebasestorage.googleapis.com/v0/b/heds-104d8.appspot.com/o/spline%2Fcloner-motion-infrared.mp4?alt=media&token=a4b7fcc3-8b5f-4f12-92af-9ec1a0d56036";
